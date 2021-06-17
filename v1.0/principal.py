8e45cc86cfbdcf53caa0ed61b502e055f24a1d7d
CristianCM4 <80124431+CristianCM4@users.noreply.github.com>
2021-06-17 18:19:39 -0400

    Update principal.py

--- v1.0/principal.py
+++ v1.0/principal.py
@@ -1,1 +1,11 @@
 # Ventana principal. Programar transiciones aqui
+from pantallas import menu_principal, tablero
+
+pantalla = pygame.display.set_mode()
+
+pantalla_actual = "menu_principal"
+while True:
+  if pantalla_actual == "menu_principal":
+    pantalla_actual = menu_principal()
+  elif pantalla_actual == "trablero":
+    pantalla_actual = tablero()
