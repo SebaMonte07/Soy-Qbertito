b16585633b02e88ded8f352e6616eb70101b22a3
pipe02 <85969736+pipe02@users.noreply.github.com>
2021-07-03 17:54:11 -0400

    Update instrucciones.py

--- v1.0/pantallas/instrucciones.py
+++ v1.0/pantallas/instrucciones.py
@@ -1,1 +1,46 @@
 # Felipe
+
+import sys
+import pygame
+
+escena_instrucciones = pygame.image.load("C:/Users/usuario/Desktop/fondo_instrucciones2.jpg")
+escena_instrucciones= pygame.transform.scale(escena_instrucciones,(650*5//3 , 650))
+
+def main(pantalla, clock):
+    i = 0
+    while True: 
+        i += 1
+        if i == 20*60:   #segundos para leer las instrucciones 20 seg
+            return "tablero"
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            elif event.type == pygame.KEYDOWN:
+                return "tablero"  #para apretar cualquier letra y pasar a la siguiente pantalla
+     
+        # DIBUJO 
+        pantalla.blit(escena_instrucciones, (0, 0))
+        
+        # ACTUALIZACION DE PANTALLA
+        pygame.display.flip()
+        clock.tick(60)
+
+##############################################################################
+
+if __name__ == "__main__":
+    pygame.init()
+    
+    pantalla = pygame.display.set_mode((650*5//3, 650))
+    clock = pygame.time.Clock()
+    
+    try:
+        main(pantalla, clock)
+    finally:
+        pygame.quit()
+
+
+
+
+
