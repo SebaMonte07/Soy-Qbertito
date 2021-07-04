6972068d23b6e917bd0835ce251e473991012890
CristianCM4 <80124431+CristianCM4@users.noreply.github.com>
2021-07-04 00:35:49 -0400

    Update principal.py

--- v1.0/principal.py
+++ v1.0/principal.py
@@ -1,11 +1,43 @@
 # Ventana principal. Programar transiciones aqui
-from pantallas import menu_principal, tablero
 
-pantalla = pygame.display.set_mode()
+import pygame
+from pantallas import menu_principal, instrucciones, tablero, pantalla_resultados, menu_puntajes
 
-pantalla_actual = "menu_principal"
-while True:
-  if pantalla_actual == "menu_principal":
-    pantalla_actual = menu_principal()
-  elif pantalla_actual == "trablero":
-    pantalla_actual = tablero()
+pygame.init()
+
+pantalla = pygame.display.set_mode((650*5//3, 650))
+clock = pygame.time.Clock()
+
+def qbertito(pantalla, clock):
+    pantalla_actual = "instrucciones"
+    
+    while True:
+        if pantalla_actual == "menu_principal":
+            break
+        
+        elif pantalla_actual == "instrucciones":
+            pantalla_actual = instrucciones.main(pantalla, clock)
+        
+        elif pantalla_actual == "historia":
+            break
+        
+        elif pantalla_actual == "tablero":
+            pantalla_actual = tablero.main(pantalla, clock)
+            
+        elif pantalla_actual == "victoria":
+            pantalla_actual = pantalla_resultados.victoria(pantalla, clock)
+            
+        elif pantalla_actual == "derrota":
+            pantalla_actual = pantalla_resultados.derrota(pantalla, clock)
+            
+        elif pantalla_actual == "puntajes":
+            pantalla_actual = menu_puntajes.MejoresPuntajes(pantalla, clock)
+            
+
+try:
+    qbertito(pantalla, clock)
+    
+finally:
+    pygame.quit()
+    
+
